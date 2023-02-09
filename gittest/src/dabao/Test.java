088453615b3a5fa0b1b447c7cae80a78e53e4d9f
simon66 <simon41866@gmail.com>
2023-02-09 16:08:59 +0000

    first local commit

---
+++ gittest/src/dabao/Test.java
@@ -0,0 +1,19 @@
+package dabao;
+
+/**
+ * Test
+ * dabao
+ * 6galaxy6
+ * 2023 02091550
+ * 2023/2/915:50
+ *
+ * @ author killjoy
+ * description:不可描述
+ **/
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("win");
+        System.out.println(" sd asd ");
+    }
+}
